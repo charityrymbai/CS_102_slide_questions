9f5b53b051d08617a4d802eaf3a1872d8d5136bd
Charity Rymbai <charityrymbai@hotmail.com>
2024-05-13 11:42:32 +0530

    Updated 4_pattern4.cpp in the patterns folder

--- Patterns/4_pattern4.cpp
+++ Patterns/4_pattern4.cpp
@@ -9,16 +9,16 @@ int main() {
     int n;
     printf("Enter the no of terms: ");
     scanf("%d", &n);
-    for (int i = 1, count = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++, count++) {
-            if (j == 1) {
-                int tabs = n - i;
-                while (tabs != 0) {
-                    printf("\t");
-                    tabs--;
-                }
-            }
+    int count = 1, tabs;
+    for (int i = 1; i <= n; i++) {
+        tabs = n - i;
+        while (tabs != 0) {
+            printf("\t");
+            tabs--;
+        }
+        for (int j = 1; j <= i; j++) {
             printf("\t%d", count);
+            count++;
         }
         printf("\n");
     }
