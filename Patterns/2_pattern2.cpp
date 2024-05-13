4bd340c2ee71dd2be22764882556f80b4058d269
Charity Rymbai <charityrymbai@hotmail.com>
2024-05-13 11:25:08 +0530

    Updated 2_pattern2.cpp in the patterns folder

--- Patterns/2_pattern2.cpp
+++ Patterns/2_pattern2.cpp
@@ -9,9 +9,11 @@ int main() {
     int n;
     printf("Enter the no of terms: ");
     scanf("%d", &n);
-    for (int i = 1, count = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++, count++) {
-            printf("\t%d", count);
+    int  count = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d\t", count);
+            count++;
         }
         printf("\n");
     }
